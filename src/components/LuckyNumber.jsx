9c17a4166e5d937bced70ea427b6e2308dfbce6b
julianasoares77 <julianaasoares77@gmail.com>
2025-06-26 18:42:23 +0000

    aula 26/06

--- src/components/LuckyNumber.jsx
+++ src/components/LuckyNumber.jsx
@@ -1,23 +1,39 @@
 import { useState } from 'react';
 import styles from './LuckyNumber.module.css';
-import { MailCheck } from 'lucide-react';
 
 export function LuckyNumber() {
-    const [luckyNumber, setLuckyNumber ] = useState(0);
-    
-    function handleClink(){
-        setLuckyNumber(Math.ceil(Math.random() * 31));
+    const [luckyNumber, setLuckyNumber] = useState(0);
+    const [history, setHistory] = useState([]);
+    const [error, setError] = useState('');
+
+    function handleClick() {
+        const newNumber = Math.ceil(Math.random() * 20);
+        setLuckyNumber(newNumber);
+        if (history.includes(newNumber)) {
+            setError('Já foi sorteado!');
+        } else {
+            setHistory([...history, newNumber]);
+            setError('');
+        }
     }
+
     return (
         <div className={styles.container}>
-            {LuckyNumber ? (
+            {luckyNumber ? (
                 <h1>Lucky Number = {luckyNumber}</h1>
-            ) : ( 
-                <h1>Lucky Number 🎲 </h1>
+            ) : (
+                <h1>Lucky Number 👀</h1>
             )}
-            <button className={styles.button} onClick={handleClink}>
-            I'm feeling lucky today!
+            <button className={styles.button} onClick={handleClick}>
+                i'm feeling lucky today!
             </button>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            {history.length > 0 && (
+                <div>
+                    <h3>Lucky Numbers Array:</h3>
+                    <p>[{history.join(', ')}]</p>
+                </div>
+            )}
         </div>
     );
 }
