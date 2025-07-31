e81a1173d7bc4f54d8d3af7efdcd9654e238accd
julianasoares77 <julianaasoares77@gmail.com>
2025-07-31 16:24:29 +0000

    desafio 7

--- src/App.jsx
+++ src/App.jsx
@@ -7,46 +7,6 @@ import { Route, Routes } from "react-router";
 import { Cart } from "./components/Cart";
 
 export default function App() {
-  
-  const [cart, setCart] = useState([]);
-
-  function addToCart(product) {
-    setCart((prevCart) => {
-      const exists = prevCart.find((item) => item.id === product.id);
-      if (exists) {
-        return prevCart.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      } else {
-        return [...prevCart, { ...product, quantity: 1 }];
-      }
-    });
-  }
-
-  function increaseQuantity(id) {
-    setCart((prevCart) =>
-      prevCart.map((item) =>
-        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-      )
-    );
-  }
-
-  function decreaseQuantity(id) {
-    setCart((prevCart) =>
-      prevCart
-        .map((item) =>
-          item.id === id ? { ...item, quantity: item.quantity - 1 } : item
-        )
-        .filter((item) => item.quantity > 0)
-    );
-  }
-
-  function clearCart() {
-    setCart([]);
-  }
-
   return (
     //React Fragment
     <>
